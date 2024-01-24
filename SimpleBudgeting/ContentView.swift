fd906ce5d501ade87a4f6b356be8f896dc8dcd5f
roijacob <roijac2b@outlook.com>
2024-01-24 11:53:23 +0800

    Initial Commit

---
+++ SimpleBudgeting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleBudgeting
+//
+//  Created by Roi Jacob C. Olfindo on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleBudgeting/SimpleBudgetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBudgetingApp.swift
+//  SimpleBudgeting
+//
+//  Created by Roi Jacob C. Olfindo on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBudgetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
